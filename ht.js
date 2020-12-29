b252958e7d5af28498c97da46d5c39df47bec123
Muchen He <i@muchen.ca>
2020-12-28 23:54:26 -0700

    ht done

--- ht.js
+++ ht.js
@@ -1,3 +1,5 @@
+const SQRT_2 = 1.4142135;
+
 let N = 512;
 
 let mX;
@@ -5,8 +7,12 @@ let mY;
 
 let I;
 
+let M;
+let L;
+
+let acc;
+
 function setup() {
-    console.log(N);
     let canvas = createCanvas(800, 800);
     canvas.parent("main");
 
@@ -71,16 +77,33 @@ function createEmptyImage(n) {
     return I
 }
 
+function createEmptyAcc(m, l) {
+    let I = new Array(m);
+    for (let i = 0; i < I.length; i++) {
+        I[i] = new Array(l);
+        for (let j = 0; j < I[i].length; j++) {
+            I[i][j] = 0;
+        }
+    }
+
+    return I
+}
+
 function onInputImageSizeChange() {
     N = document.getElementById("imageWidth").value;
     I = createEmptyImage(int(N));
-    console.log(I);
+
+    /* Update hough accumulator size */
+    L = round(SQRT_2 * N);
+    M = round(SQRT_2 * 2 * N);
+    document.getElementById("hough-size-width").innerHTML = L;
+    document.getElementById("hough-size-height").innerHTML = M;
 }
 
 function mouseClicked() {
 
     /* Toggle the pixel */
-    if (mouseInBound) {
+    if (mouseInBound()) {
         if (I[mY][mX] > 0)
             I[mY][mX] = 0;
         else
@@ -98,4 +121,49 @@ function drawInputImage() {
     }
     img.updatePixels();
     image(img, 0, 0, width, height);
+}
+
+function ht_inputStationary() {
+    
+    /* Make accumulator */
+    let acc = createEmptyAcc(M, L);
+    const deltaT = PI / L;
+    for (let i = 0; i < N; i++) {
+        for (let j = 0; j < N; j++) {
+            for (let v = 0; v < L; v++) {
+                if (I[j][i] == 0)
+                    continue;
+                
+                let theta = deltaT * v - HALF_PI;
+                let rho = i * Math.cos(theta) + j + Math.sin(theta);
+                let u = floor(rho + (M / 2));
+                if (u >= 0 && u < M)
+                    acc[u][v]++;
+            }
+        }
+    }
+
+    /* Find max value */
+    let max = 0;
+    for (let u = 0; u < M; u++) {
+        for (let v = 0; v < L; v++) {
+            if (acc[u][v] > max)
+                max = acc[u][v];
+        }
+    }
+    console.log('max value', max);
+
+    /* Normalize and write to output */
+    let accI = createImage(L, M);
+    accI.loadPixels();
+    for (let u = 0; u < M; u++) {
+        for (let v = 0; v < L; v++) {
+            accI.set(v, u, acc[u][v] * (255 / max));
+        }
+    }
+    accI.updatePixels();
+
+    /* Write to DOM */
+    let accImg = createImg(accI.canvas.toDataURL(), '');
+    accImg.parent("output-div")
 }
